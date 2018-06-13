bde4deb07437c793e88b234c19458c3cc45579f6
geetika <geetika2598@gmail.com>
2018-06-12 21:39:57 -0700

    assignment-1 and assignment-2 completed

---
+++ assignment-2.py
@@ -0,0 +1,51 @@
+#Q1 Print anything you want on screen 
+print("this is my second assignment project in the series of learning python.\n")
+print("python is meant to be easily readable lanagauge\n")
+print("its does not use curly brakets and semicolons after statements are optional\n")
+print("The syntax of the language is clean and length of the code is relatively short.")
+print("It's fun to work in Python because it allows you to think about the problem rather than focusing on the syntax.")
+print("To represent a statement in Python, newline (enter) is used.")
+print("The use of semicolon at the end of the statement is optional (unlike languages like C/C++, JavaScript, PHP).")
+
+print("\n")
+
+#Q2 Join two string using '+' E.g.-"Acad"+"View"
+x='"Acad"'
+y='"View"'
+print(x,"+",y)
+
+print("\n")
+
+#Q3 Take the input of 3 variable X,y and z.Print their values on screen.
+x=int(input("enter first value "))
+y=int(input("enter second value "))
+z=int(input("enter third value "))
+print(x,y,z)
+
+print("\n")
+ 
+#Q4 Print"Let's get started" on screen".
+print('"Let\'s get started"')
+
+print("\n")
+
+#Q5 Print the following values using Placeholders. s="Acadview course="Python" Fees=5000.
+x="Acadview Course="
+y="Python"
+z="fee=5000"
+print("%s %s %s" %(x,y,z))
+
+print("\n")
+
+#Q6 Let's do some interesting exercise: name="tony stark" salary= 1000000  print('%s''%d')%(___,___)
+name="tony stark"
+salary=1000000
+print("%s , %d "%(name,salary))
+
+
+
+
+
+
+
+
